5488ef94109e59c1b900fcd16da06efbadb40d84
rafaelg2 <rafaelg2@cisco.com>
2020-08-11 13:43:42 -0500

    meraki-update

--- python_1/DevNet/Meraki/meraki-sdk.py
+++ python_1/DevNet/Meraki/meraki-sdk.py
@@ -6,8 +6,24 @@ token = '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'
 meraki = MerakiSdkClient(token)#constructor
 
 orgs = meraki.organizations.get_organizations()
+#print(orgs)
 
 for org in orgs:
-   if org["name"]== "DevNet Sandbox":
-        org_id = org["id"]
-        print(org_id)
+    if org["name"]== "DevNet Sandbox":
+        orgID = org["id"]
+    #print(orgID)
+    
+
+params = {}
+params['organization_id'] = orgID
+networks = meraki.networks.get_organization_networks(params)
+#pprint(networks)
+
+for network in networks:
+    if network["name"] == 'DevNet Sandbox ALWAYS ON':
+        network_Id = network["id"]
+        pprint(network_Id)
+"""
+vlans = meraki.vlans.get_network_vlans(network_Id)
+pprint(vlans)
+"""
